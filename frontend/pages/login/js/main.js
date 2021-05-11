a304be622afe448825fa75cc0fcd761550170126
AfonsoAbreu <65315866+AfonsoAbreu@users.noreply.github.com>
2021-05-10 22:36:18 -0300

    Added auto-login support

--- frontend/pages/login/js/main.js
+++ frontend/pages/login/js/main.js
@@ -3,28 +3,43 @@ import labelFloat from "../../../public/js/labelFloat.js";
 const inputs = [...labelFloat()];
 
 const signIn = document.getElementById("signIn");
-signIn.addEventListener("click", async function () {
-  const response = await fetch(
-    "./api/user", 
-    {
-      method: "POST",
-      credentials: "include",
-      headers: {
-        "Accept": "*/*",
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*"
-      },
-      body: JSON.stringify({
-        email: inputs[0].value,
-        password: inputs[1].value
-      })
-    }
-  );
-
-  if (response.status !== 200) {
+signIn.addEventListener("click", async () => {
+  const success = await tryLogin("./api/user", "POST", {
+    email: inputs[0].value,
+    password: inputs[1].value
+  });
+  if (success) {
+    window.location = "gerenciar-itens";
+  } else {
     const err = document.getElementById("err");
     err.textContent = "Dados Inválidos";
-  } else {
-    window.location = "gerenciar-itens";
   }
 });
+
+async function tryLogin (route, method, body = {}) {
+  const reqData = {
+    method,
+    credentials: "include",
+    headers: {
+      "Accept": "*/*",
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    },
+  };
+  if (method === "PUT" || method === "POST" || method === "PATCH") {
+    reqData.body = body
+  }
+  const response = await fetch(
+    route, 
+    reqData
+  );
+  
+  return (response.status === 200);
+}
+
+window.onload = async () => {
+  const success = await tryLogin("./api/user", "GET");
+  if (success) {
+    window.location = "gerenciar-itens";
+  }
+}
